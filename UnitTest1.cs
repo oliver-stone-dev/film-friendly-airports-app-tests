6d37dce21c2271ab26bab6332d16e61631198a76
oliver-stone-dev <oliver.stone.dev@outlook.com>
2024-11-20 10:02:12 +0000

    Initial commit

---
+++ UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace film_friendly_airports_app_tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
